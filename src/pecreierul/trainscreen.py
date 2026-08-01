c1fc5257ce59c10ba29bbcff90943e9c5261ee84
Maik <mikehardice@gmail.com>
2026-08-01 21:30:34 +0100

    initial trainings screen

--- src/pecreierul/trainscreen.py
+++ src/pecreierul/trainscreen.py
@@ -1,7 +1,12 @@
 import os;
 
+from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+
+from pecreierul.app import PeCreierulBaseApp
 
 Builder.load_file(os.path.join(os.path.dirname(__file__), 'trainscreen.kv'))
 
@@ -9,4 +14,21 @@ class TrainLessonScreen(Screen):
 
     lesson_id: int = -1
 
+    app: PeCreierulBaseApp | None
+
+    def back(self):
+
+        if self.app is None:
+            return
+
+        self.app.manager.current = "main_menu"
+
+    def on_pre_enter(self, *args):
+
+        self.app = App.get_running_app()
+
+class TextAnswerBox(BoxLayout):
+    pass
+
+class MultipleChoiceAnswerBox(BoxLayout):
     pass
